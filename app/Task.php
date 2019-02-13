3598b36480333fca7ec367782b806aab7f489c90
Sergio S <theLastRkoch@gmail.com>
2019-02-12 21:09:49 -0600

    Masked the dates

--- app/Task.php
+++ app/Task.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\Types\Nullable;
 
 class Task extends Model
 {
@@ -16,4 +17,20 @@ class Task extends Model
         'EndDate',
         'PeriodTime'
     ];
+
+    public function getStartDateAttribute($value){
+        $MaskedValue = date("Y-m-d",strtotime($value));
+        if($MaskedValue == "1970-01-01"){
+            return "";
+        }
+        return $MaskedValue;
+    }
+
+    public function getEndDateAttribute($value){
+        $MaskedValue = date("Y-m-d",strtotime($value));
+        if($MaskedValue == "1970-01-01"){
+            return "";
+        }
+        return $MaskedValue;
+    }
 }
