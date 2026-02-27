d64936e562e9187ee97db4dcd6437427cfaa3118
Sergio S <thelastrkoch@gmail.com>
2026-02-26 23:06:02 -0600

    Working on html scraper

--- src/services/scraper.py
+++ src/services/scraper.py
@@ -1,4 +1,10 @@
 class ScraperService:
 
-    def scrape_html(self):
-        pass
+    def get_invoice_details(self, vendor):
+        match command:
+          case "BAC":
+              print("Start processing "+vendor)
+          case "WINK":
+              print("Start processing "+vendor)
+          case _:
+              print("Start processing "+vendor)
